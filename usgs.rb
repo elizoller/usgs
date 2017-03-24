93470fcc1a89f4f20eb4fd055070b0f3fedabba8
Eli Zoller <zoller.eli@gmail.com>
2017-03-24 17:50:50 -0400

    initial commit - just a starting script which loops through each of the states and gets all the site codes then from there the more detailed calls can be made

---
+++ usgs.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+
+class GetUSGSData
+  def initialize
+    # site says 100,000 limit at a time
+    # no known documentation on how to "paginate" or get the next 100,000 so we can't return more
+
+    state_codes = ["al","ak","aq","az","ar","ca","co","ct","de","dc","fl","ga","gu","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","mp","oh","ok","or","pa","pr","ri","sc","sd","tn","tx","ut","vt","vi","va","wa","wv","wi","wy"]
+
+    # lets start by getting all the sites we can loop through and get the data for each site
+    state_codes.each do |state|
+      puts state
+      params = { :stateCd => state }
+
+      # http://waterservices.usgs.gov/nwis/site/?stateCd=ny by state
+      response = get_http_response('https://waterservices.usgs.gov/nwis/site', params)
+
+      doc = response.body
+      doc_array = response.body.split("\n")
+      # then loop return and get each of the site codes
+      doc_array.each do |line|
+        # this means its a line with valuable data in it
+        if line.start_with?("USGS")
+          line_array = line.split("\t")
+          site_code = line_array[1] # this assumes that the second column is always the site code
+          puts site_code #just outputting this for now
+          # now we have the site code so we can do stuff with it, like get the daily values or the instaneaous values or the statistics values (see options here: https://waterservices.usgs.gov/rest/), the difficulty with these is that there is a ton of data so you need to do it by site_code and then probably do it by dates to limit the amount of data coming back
+          # the instaneaous values only go back to Oct 1 2007 - https://waterservices.usgs.gov/rest/IV-Service.html, need params like http://waterservices.usgs.gov/nwis/iv/?site=904027710000007&startDt=2017-01-01&endDt=2017-01-01
+          # the daily values (not sure how far these go back) - http://waterservices.usgs.gov/nwis/dv/?site=01646500&startDt=2017-01-01&endDt=2017-01-01
+        end
+      end
+    end
+  end
+
+  def get_http_response(uri, params={})
+    uri = URI(uri)
+    if !params.empty?
+      uri.query = URI.encode_www_form(params)
+    end
+    response = Net::HTTP.get_response(uri)
+    if response.is_a?(Net::HTTPRedirection)
+      response = get_http_response(response['location'])
+    end
+    # should probably set a cap for the number of redirections also
+    if response.is_a?(Net::HTTPSuccess)
+      return response
+    else
+      return false
+    end
+  end
+
+end
+
+GetUSGSData.new
